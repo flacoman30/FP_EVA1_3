628a012561e1e9bb344d72cdb6c9e6db2d47f476
flacoman30 <L24550329@chihuahua2.tecnm.mx>
2024-08-28 16:17:32 -0600

    Add files via upload

---
+++ EVA1_3_AUTOMOVIL.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_3_automovil;
+
+/**
+ *
+ * @author flac3
+ */
+public class EVA1_3_AUTOMOVIL {
+
+    public static void main(String[] args) {
+        //TODO CODE APPLICATION LOGIC HERE
+        //decraracion de las variables
+        String marca;
+        String modelo;
+        int year;
+        double precio;
+        
+        //ASIGNAR VALORES
+        marca = "ford";
+        modelo = "Mustang";
+        year = 2022;
+        precio = 999000.1;
+        System.out.println(marca);
+        System.out.println(modelo);
+        System.out.println(year);
+        System.out.println(precio); //EVA3
+        
+             
+        
+        
+        
+    }
+}
